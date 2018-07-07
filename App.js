09daab03bb2da8034032434b93b52c73583e4d72
Thanyaluk <thanyaluk@fanduel.com>
2018-07-07 23:57:06 +0100

    run project on android sim change text and colour in App.js

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ export default class App extends Component<Props> {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js - Hello world!
+          Hello world!
         </Text>
         <Text style={styles.instructions}>
           {instructions}
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5C',
+    backgroundColor: '#ffe564',
   },
   welcome: {
     fontSize: 20,
